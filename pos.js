4ad17b8db1bd42139cacfbd099d0da8ce0603073
ninrin <n_huang@hotmail.fr>
2020-02-07 14:26:51 +0100

    sa te feras 10€

--- pos.js
+++ pos.js
@@ -44,7 +44,7 @@ for (let colls of coll) {
     if (content.style.display === "block") {
       content.style.display = "none";
     }  else {
-      content.style.display = "block";
+      content.style.display = "grid";
 
     };
   });
