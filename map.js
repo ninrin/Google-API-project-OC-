bb15bb68985e82c7876d1edeae909f39e783200f
Nicolas Huang <n_huang@hotmail.fr>
2020-07-30 04:19:27 +0200

    commit

--- map.js
+++ map.js
@@ -9,235 +9,145 @@ function initMap (){
 	 
 	
 
-if (navigator.geolocation) //  renvoie un simple booléen valant vrai ou faux selon la capacité du navigateur à utiliser la géolocalisation 
-{
-    navigator.geolocation.getCurrentPosition(function(position) // methode .getCurrentPosition sert à récupérer les coordonnées de l'utilisateur
-    {
-		var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+	if (navigator.geolocation) {//  renvoie un simple booléen valant vrai ou faux selon la capacité du navigateur à utiliser la géolocalisation 
+		
+		navigator.geolocation.getCurrentPosition(function(position){ // methode .getCurrentPosition sert à récupérer les coordonnées de l'utilisateur	
+			var pos = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+			};
 			map.setCenter(pos);
-			var marker = new google.maps.Marker(
-			{
+			var marker = new google.maps.Marker({
 				position: pos,
 				map: map,
 				icon: 'http://maps.google.com/mapfiles/kml/paddle/ylw-blank.png'
 			});
-	},function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
+		},function() {
+		  handleLocationError(true, infoWindow, map.getCenter());
+		});
+	
+	} else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+	}
 
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+	function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+		infoWindow.setPosition(pos);
+		infoWindow.setContent(browserHasGeolocation ?
+							'Error: The Geolocation service failed.' :
+							'Error: Your browser doesn\'t support geolocation.');
 
-}
-
-
-console.log(lati1);
-console.log(restaurants);
-
-var neighborhoods = [ {lat: lati1, lng: longi1},  {lat: lati2, lng: longi2}];
+	}
 
 
+	console.log(lati1);
+	console.log(restaurants);
 
+	var neighborhoods = [ {lat: lati1, lng: longi1},  {lat: lati2, lng: longi2}];
 
-
-  for (var i = 0; i < neighborhoods.length; i++) {
+	for (var i = 0; i < neighborhoods.length; i++) {
 	  
-    placeMarker(map, neighborhoods[i], arRestaurants[i][0]);
-	img(arRestaurants[i][4],arRestaurants[i][5])
-addLi(arRestaurants[i][0], arRestaurants[i][0], streetView, arRestaurants[i][1] , arRestaurants[i][2]);
-
-    
-  }
-  
+		placeMarker(map, neighborhoods[i], arRestaurants[i][0]);
+		img(arRestaurants[i][4],arRestaurants[i][5])
+		addLi(arRestaurants[i][0], arRestaurants[i][0], streetView, arRestaurants[i][1] , arRestaurants[i][2]);  
+	}
   
 
 
+	async function placeMarker(map, locat,title) { // place les markers saisi manuellement
+		var marker = new google.maps.Marker({
+		position: locat,
+		map: map,		
+		title: title
+	});  
+
+		var latitude =  locat.lat();
+		var longitude =  locat.lng();
+		console.log(latitude);
+		geocode(latitude, longitude);
+		img(latitude, longitude);
+	}
 
 
-
-
-async function placeMarker(map, locat,title) {
-	var marker = new google.maps.Marker({
-	position: locat,
-	map: map,		
-	title: title
-});  
-
-	var latitude =  locat.lat();
-	var longitude =  locat.lng();
-	console.log(latitude);
-	geocode(latitude, longitude);
-	img(latitude, longitude);
-}
-
-
-function placePlaceMarker(map, position){
-	service= new google.maps.places.PlacesService(map);
-
-
-request = {
-	location: position,
-	radius: 800,
-	types: ['restaurant']
-	}; 
-		
-	service.nearbySearch(request, callback); 
 	
-	google.maps.event.addListener(map, 'rightclick', function(event) {
-		map.setCenter(event.latLng)
-		
-		clearResults(markers) 
-		$("#list").empty();
-		var request={
-			location: event.LatLng,
-			radius: 800, 
-			types: ['restaurant']
-			};
-			service.nearbySearch(request, callback);
-	})
-
- function callback(results, status) { 
 
 
- if (status == google.maps.places.PlacesServiceStatus.OK){
-	 for (var i = 0; i < results.length; i++) {
-		
-		 markers.push(createMarker(results[i]));
-		  var place= results[i];
-
-			var lat = place.geometry.location.lat();
-			var lng = place.geometry.location.lng();
-			img(lat,lng);
-			addLi(place.name, place.name,streetView, place.rating, place.vicinity);
-
-		 }
-			toggle();
-		 
-}
-};
-
-		function createMarker(place) { 
-		var placeLoc = place.geometry.locatio; 
-		var marker= new google.maps.Marker({
-			map: map, 
-			position: place.geometry.location,
-			title: place.name,
-			}); 
-	
-			google.maps.event.addListener(marker, 'click', function(){
-				infoWindow.open(map, marker);
-				});
-				return marker;
+	google.maps.event.addListener(map, 'click', function(event) {
+		var txt;
+		var restaurantName = prompt("Please enter restaurant name:");
+		if (restaurantName == null || restaurantName == "") {
+			txt = "User cancelled the prompt.";
+		} else {
+			txt =  restaurantName;
+			geocode(event.lat, event.lng);
+			addLi(restaurantName, restaurantName,streetView,'', loc);
+			placeMarker(map, event.latLng,restaurantName);
+		placePlaceMarker(map, event.latLng);	  			  
 		}
-		
-		function clearResults(markers) {
-			for (var m in markers) {
-				markers[m].setMap(null)
-				}
-				markers= []
-				
-				} 
-				
-};
-
-
-  google.maps.event.addListener(map, 'click', function(event) {
-   var txt;
-  var restaurantName = prompt("Please enter restaurant name:");
-  if (restaurantName == null || restaurantName == "") {
-    txt = "User cancelled the prompt.";
-  } else {
-    txt =  restaurantName;
-	geocode(event.lat, event.lng);
-	addLi(restaurantName, restaurantName,streetView,'', loc);
-	  placeMarker(map, event.latLng,restaurantName);
-	  placePlaceMarker(map, event.latLng);	
-	  
-	   
-
-}
-
-console.log(markers);
- 
-})
+		console.log(markers);	 
+	})
 
 }			
 
 
 
-var c;
+var idCliqué;
 var rate;
 var valeurSpan2= [];
 
 
-
-
 function confirmer(){
-var valeur;//récupérer la valeur d'un bouton radio correspondant à la note
-		var optradioRate = document.getElementsByName("optradioRate");
-		for (var i = 0; i < optradioRate.length; i++) {
-			if (optradioRate[i].checked ) {
-				valeur = optradioRate[i].value;
-				console.log(valeur);
+	var valeur;//récupérer la valeur d'un bouton radio correspondant à la note
+			var optradioRate = document.getElementsByName("optradioRate");
+			for (var i = 0; i < optradioRate.length; i++) {
+				if (optradioRate[i].checked ) {
+					valeur = optradioRate[i].value;
+					console.log(valeur);
+				}
 			}
-		}
-		var input = document.getElementById("exampleFormControlTextarea1").value; // valeur dela note
-		console.log(input);	
+			var input = document.getElementById("exampleFormControlTextarea1").value; // valeur dela note
+			console.log(input);	
 
-var par = $(c).parent(); // le DIV parent de l'id du boutton cliqué
-var content = par.find(".content"); // cible la class content qui contient les commentaires et notes
-var allSpan= content.find("span"); // cible les span dans content 
-content.append(`<p>${input} <span class =${valeur}> note: ${valeur} </span></p>`);
-let note =  par.find('.note').text();
-console.log(note);
-var valeurSpan; // valeur de la class du span
-var valeurSpan2= []; // tableau contenant toute les valeurs (en string) des class des span
-valeurSpan2.push(note);
-for (let span of allSpan) {
-			if (allSpan) {
-				valeurSpan = span.className;
-				valeurSpan2.push(valeurSpan);
-				let finale = valeurSpan2.map(numStr => parseInt (numStr)); // on change les string en numérique pour chaque valeur du array 
-				rate = finale.reduce(reducer)/valeurSpan2.length;
-				par.find('.note').text(rate);
-			}	
-		}
-console.log(valeurSpan2);
-console.log(valeurSpan);
-
-
- 
+	var par = $(idCliqué).parent(); // le DIV parent de l'id du boutton cliqué
+	var content = par.find(".content"); // cible la class content qui contient les commentaires et notes
+	var allSpan= content.find("span"); // cible les span dans content 
+	content.append(`<p>${input} <span class =${valeur}> note: ${valeur} </span></p>`);
+	let note =  par.find('.note').text();
+	console.log(note);
+	var valeurSpan; // valeur de la class du span
+	var valeurSpan2= []; // tableau contenant toute les valeurs (en string) des class des span
+	valeurSpan2.push(note);
+	for (let span of allSpan) {
+				if (allSpan) {
+					valeurSpan = span.className;
+					valeurSpan2.push(valeurSpan);
+					let finale = valeurSpan2.map(numStr => parseInt (numStr)); // on change les string en numérique pour chaque valeur du array 
+					rate = finale.reduce(reducer)/valeurSpan2.length;
+					par.find('.note').text(rate);
+				}	
+			}
+	console.log(valeurSpan2);
+	console.log(valeurSpan);
+  
 }
 
 
-$('#liste').on('dblclick', 'button', function() {
+$('#liste').on('dblclick', 'button', function() { // 
 
     var id = $(this).attr('id'); // l'id du boutton cliqué (en valeur seulement)
     console.log(id);  
     $("#myModal").modal();
-	 c = document.getElementById(id); // cible l'id du boutton
-	 var pari = $(c).parent().find(".note").text(); // permet de récuperer 
-	 var num = Number(pari);
-	 console.log(num);
-	 console.log(pari);
+	idCliqué = document.getElementById(id); // cible l'id du boutton
+	var globalRate = $(idCliqué).parent().find(".note").text(); // permet de récuperer la note générale
+	var num = Number(globalRate);
+	console.log(num);
+	console.log(globalRate);
 	 
-if (pari === 'undefined'){
-		console.log('yoyo');
-	}
-	
-	
-	 // console.log(pari);
+	if (globalRate === 'undefined'){
+			console.log('yoyo');
+	}		
+		
 })
 	
 
--- pos.js
+++ pos.js
@@ -2,28 +2,28 @@ var streetView;
 var loc;
 const reducer = (accumulator, currentValue) => accumulator + currentValue; // opération permettant de prendre la valeur totale 
 
-  function addLi(inner, restaurantName, url, valeur, adresse) {
-  var ul = document.getElementById("list");
-  var img = document.createElement("img"); 
-  img.src = url;
-  var divs = document.createElement("div"); 
-  divs.setAttribute("class", restaurantName);
-var btn = document.createElement("BUTTON");                            
-btn.setAttribute("type", "boutton");
-btn.setAttribute("id", restaurantName);
-btn.classList.add("collapsible");
-let ifValeur = valeur || 0 // si il y a pas de valeur, alors la valeur de note équivaut à 0
-  var txt1 = $("<span></span>").text(inner);  
-  var txt2 = $("<span></span>").addClass("note").text(ifValeur);  
-  var txt3 = $("<p></p>").text("adresse : "+ adresse);  
-$(btn).append(txt1, txt2, txt3);   // Append new elements
-ul.appendChild(divs);
-divs.appendChild(btn);
-btn.appendChild(img);
+function addLi(inner, restaurantName, url, valeur, adresse) {  // ajoute une ligne à chaque ajout de markers correspondants
+	var ul = document.getElementById("list");
+	var img = document.createElement("img"); 
+	img.src = url;
+	var divs = document.createElement("div"); 
+	divs.setAttribute("class", restaurantName);
+	var btn = document.createElement("BUTTON");                            
+	btn.setAttribute("type", "boutton");
+	btn.setAttribute("id", restaurantName);
+	btn.classList.add("collapsible");
+	let ifValeur = valeur || 0 // si il y a pas de valeur, alors la valeur de note équivaut à 0
+	var txt1 = $("<span></span>").text(inner);  
+	var txt2 = $("<span></span>").addClass("note").text(ifValeur);  
+	var txt3 = $("<p></p>").text("adresse : "+ adresse);  
+	$(btn).append(txt1, txt2, txt3);   // Append new elements
+	ul.appendChild(divs);
+	divs.appendChild(btn);
+	btn.appendChild(img);
 
-var div = document.createElement('div');
-div.setAttribute('class', 'content');
-divs.appendChild(div);
+	var div = document.createElement('div');
+	div.setAttribute('class', 'content');
+	divs.appendChild(div);
 
 }
 
@@ -31,63 +31,32 @@ divs.appendChild(div);
 
 
 var markers = []; 
+var marker = []; 
 
 
-function toggle(){ // fonction pour dérouler le callapsible et voir le content des notes et commentaires ajoutés
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-for (let colls of coll) {
-  colls.addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    }  else {
-      content.style.display = "grid";
-
-    };
-  });
+function toggle(){ // fonction pour dérouler le collapsible et voir le content des notes et commentaires ajoutés
+	var coll = document.getElementsByClassName("collapsible");
+	var i;
+	for (let colls of coll) {
+		colls.addEventListener("click", function() {
+			this.classList.toggle("active");
+			var content = this.nextElementSibling;
+			if (content.style.display === "block") {
+				content.style.display = "none";
+			}  else {
+				content.style.display = "grid";
+			};
+		});
+	}
 }
-}
 
-// const datas = [{
-        // "restaurantName": "Bronco",
-        // "address": "39 Rue des Petites Écuries, 75010 Paris",
-        // "lat": 48.8737815,
-        // "lng": 2.3501649,
-        // "ratings": [{
-                // "stars": 4,
-                // "comment": "Un excellent restaurant, j'y reviendrai ! Par contre il vaut mieux aimer la viande."
-            // },
-            // {
-                // "stars": 5,
-                // "comment": "Tout simplement mon restaurant préféré !"
-            // }
-        // ]
-    // },
-    // {
-        // "restaurantName": "Babalou",
-        // "address": "4 Rue Lamarck, 75018 Paris",
-        // "lat": 48.8865035,
-        // "lng": 2.3442197,
-        // "ratings": [{
-                // "stars": 5,
-                // "comment": "Une minuscule pizzeria délicieuse cachée juste à côté du Sacré choeur !"
-            // },
-            // {
-                // "stars": 3,
-                // "comment": "J'ai trouvé ça correct, sans plus"
-            // }
-        // ]
-    // }
-// ];
+
 
 let arRestaurants = [];
 
 for (const object of restaurants) { // la variable object prend les informations de chaque index dans de la variable array de data; dans une boucle
 
- let
+	let
         restaurant = object['restaurantName']; //restaurant reprend les noms des restaurants		
         note = null; // 
 		adresse = object['address'];
@@ -98,7 +67,7 @@ for (const object of restaurants) { // la variable object prend les informations
     for ( const rating of object['ratings'] ){
         note += Number(rating['stars']); // note contiendra les nombres total des notes de chaque index
     }
-console.log(note);
+	console.log(note);
     note = ( note / object['ratings'].length ).toFixed(1); //calcul de moyenne 
     arRestaurants.push( [ restaurant, note, adresse, commentaire, lati, longi]);
 }
@@ -125,89 +94,108 @@ let lati1 = arRestaurants[0][4];
 let longi1 = arRestaurants[0][5];
 	longi2 = arRestaurants[1][5];
 
- console.table(arRestaurants);
+console.table(arRestaurants);
 console.log(arRestaurants[0][4]);
 console.log(arRestaurants[1][0], arRestaurants[1][1]);
 
 
-const img= (latitude, longitude) => {
-	
-		 const LAT = latitude;
-		 const LNG = longitude;
-
-		  streetView =   `https://maps.googleapis.com/maps/api/streetview?size=100x100&location=${LAT},${LNG}&heading=151.78&pitch=-0.76&key=AIzaSyA95IBarLMikquqc-xUBNNL4sIpBrQv9yc`;
-
-	  }
+const img= (latitude, longitude) => { // fonction pour ajouter l'image du restaurant en fonction de lat/lng
+	const LAT = latitude;
+	const LNG = longitude;
+	streetView =   `https://maps.googleapis.com/maps/api/streetview?size=100x100&location=${LAT},${LNG}&heading=151.78&pitch=-0.76&key=AIzaSyA95IBarLMikquqc-xUBNNL4sIpBrQv9yc`;
+}
 	  
 
 
- const geocode = (lat, lng) => {
-      const LAT = lat;
-      const LNG = lng;
-      let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${LAT},${LNG}&key=AIzaSyA95IBarLMikquqc-xUBNNL4sIpBrQv9yc`;
+const geocode = (lat, lng) => {
+    const LAT = lat;
+    const LNG = lng;
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${LAT},${LNG}&key=AIzaSyA95IBarLMikquqc-xUBNNL4sIpBrQv9yc`;
 
-      fetch(url)
-        .then(response => response.json())
+    fetch(url)
+		.then(response => response.json())
         .then(data => {
-          console.log(data);
-          let parts = data.results[0].address_components;
-             loc = data.results[0].formatted_address;
+			console.log(data);
+			let parts = data.results[0].address_components;
+            loc = data.results[0].formatted_address;
 			console.log(loc);
 			console.log(parts);
 			
-infoWindow = new google.maps.InfoWindow({
-    content:  
-	"Restaurant"
-	});
+			infoWindow = new google.maps.InfoWindow({
+				content:  
+				"Restaurant"
+				});
 
-  marker.addListener('click', function() {
-    infoWindow.open(map, marker);
+			marker.addListener('click', function() {
+					infoWindow.open(map, marker);
 
-  });
+			});
 
-        })
-        .catch(err => console.warn(err.message));
+        }).catch(err => console.warn(err.message));
 
-   }
+}
    
-   
-   
-   function generate(){
-  
+function placePlaceMarker(map, position){ // place les markers des restaurants alentour
+		service= new google.maps.places.PlacesService(map);
+		
+		request = {
+			location: position,
+			radius: 800,
+			types: ['restaurant']
+		}; 
+				
+		service.nearbySearch(request, callback); 
+			
+		google.maps.event.addListener(map, 'rightclick', function(event) {
+			map.setCenter(event.latLng)	
+			clearResults(markers) 
+			$("#list").empty();
+			var request={
+				location: event.LatLng,
+				radius: 800, 
+				types: ['restaurant']
+			};
+			service.nearbySearch(request, callback);
+		})
 
-		var valeurRate2;//récupérer la valeurRate1 d'un bouton radio
-		var optradio2 = document.getElementsByName("optradio2");
-		var note=  $("#liste").find(".note");
-		var num = +note;
-		console.log(note);
-   var noteTableau = [];
+		function callback(results, status) { 
+			if (status == google.maps.places.PlacesServiceStatus.OK){
+				for (var i = 0; i < results.length; i++) {
+					markers.push(createMarker(results[i]));
+					var place= results[i];
+					var lat = place.geometry.location.lat();
+					var lng = place.geometry.location.lng();
+					img(lat,lng);
+					addLi(place.name, place.name,streetView, place.rating, place.vicinity);
+				}
+				toggle();			 
+			}
+		};
 
-for (var i = 0; i < optradio2.length; i++) {
-			if (optradio2[i].checked){
-				valeurRate2 = optradio2[i].value;
-
-			}	
+		function createMarker(place) { 
+		var placeLoc = place.geometry.locatio; 
+		var marker= new google.maps.Marker({
+			map: map, 
+			position: place.geometry.location,
+			title: place.name,
+			}); 
+	
+			google.maps.event.addListener(marker, 'click', function(){
+				infoWindow.open(map, marker);
+				});
+				return marker;
 		}
-for (var i = 0; i < note.length; i++) {
-  var price = note[i].innerText;
-  console.log("Price: " + price);
-  noteTableau.push(price);
-  }	
-  
-// console.log(noteTableau);
-	// if (valeurRate2 >= noteTableau) {
+		function clearResults(markers) {
+			for (var m in markers) {
+					markers[m].setMap(null)
+				}
+				markers= []
+				
+		} 
+				
+};
+
+
 		
-
-
-
-   // }	
-   }	
-		
-		// var valeur;//récupérer la valeur d'un bouton radio
-		// var optradio = document.getElementsByName("optradio");
-		// for (var i = 0; i < optradio.length; i++) {
-			// if (optradio[i].checked ) {
-				// valeur = optradio[i].value;
-			// }	
-		// }
+	
 	 
